4908dff8650d2040b90fd1c7b93f03b47baf3f80
macedo-developer <macedorenata.developer@gmail.com>
2020-10-12 14:39:41 -0300

    Change a to link react-router-dom

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FiArrowRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 import logo from "../images/logo.svg";
 
@@ -19,9 +20,9 @@ function Landing() {
           <span>Paraíba</span>
         </div>
 
-        <a href="/app" className="enter-app">
+        <Link to="/app" className="enter-app">
           <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
-        </a>
+        </Link>
       </div>
     </div>
   );

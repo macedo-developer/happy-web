faea0e51c414cfce4c572ce157e24a0098ed76d7
macedo-developer <macedorenata.developer@gmail.com>
2020-10-12 14:56:59 -0300

    Creating structure and styles of orphanages map

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
+import mapMarkerImg from "../images/map-marker.svg";
 
-import "../styles/pages/orphanagesMap.css";
+import "../styles/pages/orphanages-map.css";
 
 function OrphanagesMap() {
-  return <h1>Orphanages Map</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Map Marker" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando sua visita</p>
+        </header>
+
+        <footer>
+          <strong>João Pessoa</strong>
+          <span>Paraíba</span>
+        </footer>
+      </aside>
+
+      <div></div>
+      <Link to="" className="create-orphanages">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;

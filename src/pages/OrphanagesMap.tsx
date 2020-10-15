40830fe80b6da2ebfd9b639b0c8807e95615ca23
macedo-developer <macedorenata.developer@gmail.com>
2020-10-14 21:49:37 -0300

    Add marker and popup

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FiPlus } from "react-icons/fi";
+import { FiArrowRight, FiPlus } from "react-icons/fi";
 import mapMarkerImg from "../images/map-marker.svg";
 
-import { Map, TileLayer } from "react-leaflet";
+import Leaflet from "leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
 import "../styles/pages/orphanages-map.css";
 
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [48, 58],
+  iconAnchor: [24, 58],
+  popupAnchor: [170, 0],
+});
+
 function OrphanagesMap() {
-  
   return (
     <div id="page-map">
       <aside>
@@ -31,8 +38,23 @@ function OrphanagesMap() {
         style={{ width: "100%", height: "100%" }}
       >
         <TileLayer
-          url={`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+
+        <Marker position={[-7.1630565, -34.8803117]} icon={mapIcon}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Ophanages one
+            <Link to="/">
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
+
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
       </Map>
       <Link to="" className="create-orphanages">
